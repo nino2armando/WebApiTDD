144429a83cec134b53cf6c905428659792cf6997
Nino2armando <nino@armando@yahoo.com>
2013-05-05 18:38:17 -0700

    clean

---
+++ WebApiTDD.Test/Test.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Moq;
+using WebApiTDD.DataContract;
+using WebApiTDD.Domain.Models;
+using Xunit;
+
+namespace WebApiTDD.Test
+{
+    public class Test
+    {
+        [Fact]
+        public void CanMapMangerToEmployee()
+        {
+
+            var manager = new Mock<Manager>(MockBehavior.Strict);
+
+            var result = Mapper.Map<Manager, Employee>(manager.Object);
+
+            Assert.Equal(result.GetType(),typeof(Employee));
+        }
+
+        [Fact]
+        public void ClaimPrincipalTest()
+        {
+            
+        }
+    }
+}
