7c0f5fa558348a0d2711545518bbf9ed8ef16a1f
Nino2armando <nino@armando@yahoo.com>
2013-05-05 18:41:36 -0700

    cleaning up

--- WebApiTDD.Test/Test.cs
+++ WebApiTDD.Test/Test.cs
@@ -1,22 +1,7 @@
-
-
-
-
-
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using AutoMapper;
 using Moq;
-using WebApiTDD.Context.AppContext;
 using WebApiTDD.DataContract;
 using WebApiTDD.Domain.Models;
-using WebApiTDD.Repository.UnitOfWork;
-using WebApiTDD.Sevice.Services;
-
 using Xunit;
 
 namespace WebApiTDD.Test
